b33b657ab7f631ea399f88d276ce2cf5ee5df127
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-03-05 10:49:39 +0300

    Create functions MapTo and Convert

--- task2/main.go
+++ task2/main.go
@@ -1,10 +1,40 @@
 package main
 
-// Specify MapTo function here
+func MapTo(slice []int, fn func(item, index int) string) (result []string) {
+	for i, value := range slice {
+		result = append(result, fn(value, i))
+	}
+	return
+}
 
 func Convert(arr []int) []string {
-	// converting nimber code should be here
+	return MapTo(arr, func(item, index int) (res string) {
+		switch item {
+		case 1:
+			res = "one"
+		case 2:
+			res = "two"
+		case 3:
+			res = "three"
+		case 4:
+			res = "four"
+		case 5:
+			res = "five"
+		case 6:
+			res = "six"
+		case 7:
+			res = "seven"
+		case 8:
+			res = "eight"
+		case 9:
+			res = "nine"
+		default:
+			res = "unknown"
+		}
+		return
+	})
 }
 
 func main() {
+
 }
