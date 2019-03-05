afa53299f9b8dc024291ef1f8e72b4d566054c7c
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-03-05 10:42:05 +0300

    Refactor Filter

--- task1/main.go
+++ task1/main.go
@@ -1,16 +1,13 @@
 package main
 
-import "fmt"
-
 func Filter(slice []int, fn func(item, index int) bool) (result []int) {
-	for i := 0; i < len(slice); i++ {
-		if fn(slice[i], i) {
-			result = append(result, slice[i])
+	for i, value := range slice {
+		if fn(value, i) {
+			result = append(result, value)
 		}
 	}
 	return
 }
 
 func main() {
-	fmt.Println("Even", Filter([]int{1, 2, 3, 4, 5}, func(item, index int) bool { return item%2 == 0 }))
 }
--- task2/main.go
+++ task2/main.go
@@ -1,10 +1,42 @@
 package main
 
-// Specify MapTo function here
+import "fmt"
+
+func MapTo(slice []int, fn func(item, index int) string) (result []string) {
+	for i := 0; i < len(slice); i++ {
+		result = append(result, fn(slice[i], i))
+	}
+	return
+}
 
 func Convert(arr []int) []string {
-	// converting nimber code should be here
+	return MapTo(arr, func(item, index int) (res string) {
+		switch item {
+		case 1:
+			res = "one"
+		case 2:
+			res = "two"
+		case 3:
+			res = "three"
+		case 4:
+			res = "four"
+		case 5:
+			res = "five"
+		case 6:
+			res = "six"
+		case 7:
+			res = "seven"
+		case 8:
+			res = "eight"
+		case 9:
+			res = "nine"
+		default:
+			res = "unknown"
+		}
+		return
+	})
 }
 
 func main() {
+	fmt.Println(MapTo([]int{1, 2, 3, 4, 5, 10}, func(item, index int) string { return item * 2 }))
 }
