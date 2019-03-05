778fdec7d3da436717a2e4c2dfca6df5698340e6
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-03-05 10:02:46 +0300

    Create Filter

--- task1/main.go
+++ task1/main.go
@@ -1,7 +1,16 @@
 package main
 
-// Specify Filter function here
+import "fmt"
+
+func Filter(slice []int, fn func(item, index int) bool) (result []int) {
+	for i := 0; i < len(slice); i++ {
+		if fn(slice[i], i) {
+			result = append(result, slice[i])
+		}
+	}
+	return
+}
 
 func main() {
-
+	fmt.Println("Even", Filter([]int{1, 2, 3, 4, 5}, func(item, index int) bool { return item%2 == 0 }))
 }
