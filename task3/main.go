90bec4e701f8cc5d7892d93fd54cf3f38ea2452b
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-03-06 23:43:01 +0300

    Refactor ver 1

--- task3/main.go
+++ task3/main.go
@@ -10,12 +10,12 @@ import (
 	"os"
 )
 
-type Arguments map[string] string
+type Arguments map[string]string
 
 type User struct {
-	id string `json:"id"`
+	id    string `json:"id"`
 	email string `json:"email"`
-	age int `json:"age"`
+	age   int    `json:"age"`
 }
 
 func parseArgs() Arguments {
@@ -26,94 +26,86 @@ func parseArgs() Arguments {
 	flag.Parse()
 	return Arguments{
 		"operation": *operation,
-		"filename": *filename,
-		"item": *item,
-		"id": *id,
+		"filename":  *filename,
+		"item":      *item,
+		"id":        *id,
 	}
 
 }
 
-func check(e error)  {
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-
-func list(arr Arguments) (users []User) {
-	filename := arr["filename"]
-	if filename == "" {
-		check(errors.New("-fileName flag has to be specified"))
-	}
-	f, err := os.Open(filename)
-	check(err)
-	defer f.Close()
+func list(f *os.File) []byte {
 	bytes, err := ioutil.ReadAll(f)
 	check(err)
-	err = json.Unmarshal(bytes, &users)
-	check(err)
-	return
+	return bytes
 }
 
-func add(filename string, item string)  {
-	if filename == "" {
-		check(errors.New("-fileName flag has to be specified"))
+//./main.go -operation "add" -item '{\"id\": \"1\", \"email\": \"email@test.com\", \"age\": 23}' -fileName "users.json"
+
+func Perform(args Arguments, writer io.Writer) (err error) {
+	if args["filename"] == "" {
+		return fmt.Errorf("-fileName flag has to be specified")
 	}
-	if item == "" {
-		check(errors.New("-item flag has to be specified"))
+	if args["operation"] == "" {
+		return errors.New("-operation flag has to be specified")
 	}
-	f, err := os.Open(filename)
+	f, err := os.Open(args["filename"])
 	check(err)
 	defer f.Close()
-	byte, err := f.Write([]byte(item))
-	check(err)
-	fmt.Println("wrote ", byte, " bytes")
-}
-
-func remove(filename string, id string)  {
-	if filename == "" {
-		check(errors.New("-fileName flag has to be specified"))
-	}
-	if id == "" {
-		check(errors.New("-id flag has to be specified"))
-	}
-	users := list(filename)
-	for i, value := range users {
-		if value.id == id {
-			users = append(users[:i], users[i+1:]...)
+	switch args["operation"] {
+	case "add":
+		item := args["item"]
+		if item == "" {
+			return errors.New("-item flag has to be specified")
 		}
-	}
-	bytes, err := json.Marshal(users)
-	check(err)
-	f, err := os.Open(filename)
-	check(err)
-	defer f.Close()
-	byte, err := f.Write(bytes)
-	check(err)
-	fmt.Println("wrote ", byte, " bytes")
-}
-
-func findById(filename string, id string) (user User) {
-	if filename == "" {
-		check(errors.New("-fileName flag has to be specified"))
-	}
-	if id == "" {
-		check(errors.New("-id flag has to be specified"))
-	}
-	users := list(filename)
-	for _, value := range users {
-		if value.id == id {
-			user = value
+		_, err := f.Write([]byte(item))
+		check(err)
+	case "remove":
+		id := args["id"]
+		if id == "" {
+			return errors.New("-id flag has to be specified")
 		}
+		var users []User
+		err = json.Unmarshal(list(f), &users)
+		for i, value := range users {
+			if value.id == id {
+				users = append(users[:i], users[i+1:]...)
+			}
+		}
+		bytes, err := json.Marshal(users)
+		check(err)
+		err = ioutil.WriteFile(args["filename"], bytes, 0644)
+		check(err)
+	case "list":
+		users := list(f)
+		_, err := writer.Write(users)
+		check(err)
+	case "findById":
+		id := args["id"]
+		if id == "" {
+			return errors.New("-id flag has to be specified")
+		}
+		var users []User
+		err = json.Unmarshal(list(f), &users)
+		var user User
+		for _, value := range users {
+			if value.id == id {
+				user = value
+			}
+		}
+		bytes, err := json.Marshal(user)
+		check(err)
+		_, err = writer.Write(bytes)
+		check(err)
+	default:
+		return fmt.Errorf("Operation %s not allowed!", args["operation"])
 	}
 	return
-}
-
-func Perform(args Arguments, writer io.Writer) error {
-	var mapOperation map[string] func(arguments Arguments){
-		"list": list(args)
-
-	}
 
 }
 
