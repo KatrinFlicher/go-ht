ffbec23c1af878e3f781bf009ee20ab56c2036b8
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-03-22 17:34:56 +0300

    Ready commit

--- http-requests-utility/main.go
+++ http-requests-utility/main.go
@@ -10,13 +10,13 @@ import (
 )
 
 type Arguments struct {
-	Adr        string
 	Addresses  []string
 	NumRequest int
 	TimeOut    time.Duration
 }
 
 type Result struct {
+	Address      string
 	TotalRuntime time.Duration
 	AverageTime  time.Duration
 	MinRespTime  time.Duration
@@ -31,85 +31,86 @@ func (f *arrayFlags) String() string {
 }
 
 func (f *arrayFlags) Set(value string) error {
+	value = "https://" + value
 	*f = append(*f, value)
 	return nil
 }
 
-func utility(args Arguments) (results []Result) {
+func utility(args Arguments) (results []Result, arguments Arguments) {
 	client := &http.Client{
 		Timeout: args.TimeOut * time.Second,
 	}
-	//for _, value := range args.Addresses {
-	var wg sync.WaitGroup
-	wg.Add(args.NumRequest)
-	var mutex = &sync.Mutex{}
-	var countNoResp int
-	var respTime []time.Duration
-	var totalRuntime time.Duration
-	for i := 0; i < args.NumRequest; i++ {
-		go func(group *sync.WaitGroup) {
-			defer wg.Done()
-			start := time.Now()
-			resp, err := client.Get(args.Adr)
-			//resp.StatusCode
-			defer resp.Body.Close()
-			duration := time.Since(start)
-			mutex.Lock()
-			if err != nil {
-				countNoResp++
-			}
-			respTime = append(respTime, duration)
-			totalRuntime = totalRuntime + duration
-			mutex.Unlock()
-		}(&wg)
+	for _, value := range args.Addresses {
+		var wg sync.WaitGroup
+		wg.Add(args.NumRequest)
+		var mutex = &sync.Mutex{}
+		var countNoResp int
+		var respTime []time.Duration
+		var totalRuntime time.Duration
+		for i := 0; i < args.NumRequest; i++ {
+			go func(group *sync.WaitGroup) {
+				defer wg.Done()
+				start := time.Now()
+				resp, err := client.Get(value)
+				defer resp.Body.Close()
+				duration := time.Since(start)
+				mutex.Lock()
+				if err != nil {
+					countNoResp++
+				}
+				respTime = append(respTime, duration)
+				totalRuntime = totalRuntime + duration
+				mutex.Unlock()
+			}(&wg)
+		}
+		wg.Wait()
+		sort.Slice(respTime, func(i, j int) bool { return respTime[i] < respTime[j] })
+		results = append(results, Result{
+			Address:      value,
+			TotalRuntime: totalRuntime,
+			AverageTime:  time.Duration(int(totalRuntime) / len(respTime)),
+			MaxRespTime:  respTime[len(respTime)-1],
+			MinRespTime:  respTime[0],
+			CountNoResp:  countNoResp,
+		})
+		fmt.Println(results)
 	}
-	wg.Wait()
-	sort.Slice(respTime, func(i, j int) bool { return respTime[i] < respTime[j] })
-	fmt.Println(respTime)
-	results = append(results, Result{
-		TotalRuntime: totalRuntime,
-		AverageTime:  time.Duration(int(totalRuntime) / len(respTime)),
-		MaxRespTime:  respTime[len(respTime)-1],
-		MinRespTime:  respTime[0],
-		CountNoResp:  countNoResp,
-	})
-	fmt.Println(results)
-	//}
 	return
 }
 
-func printResults(results []Result) {
+func printResults(results []Result, arguments Arguments) {
 	for _, result := range results {
+		fmt.Println("Work with address ", result.Address, " is successfully done")
+		fmt.Println("Number of send requests was ", arguments.NumRequest)
+		fmt.Println("Timeout was set in ", arguments.TimeOut)
 		fmt.Println("All requests worked at ", result.TotalRuntime)
 		fmt.Println("Average time to request: ", result.AverageTime)
-		fmt.Println("Maximum return response time is ", result.MaxRespTime,
-			"Minimum return response time is ", result.MinRespTime)
+		fmt.Println("Maximum return response time is ", result.MaxRespTime)
+		fmt.Println("Minimum return response time is ", result.MinRespTime)
 		fmt.Println("The number of answers were not waited: ", result.CountNoResp)
 	}
 }
 
 func parseArgs() Arguments {
-	//var addresses arrayFlags
-	//flag.Var(&addresses, "address", "Addresses for request")
-	//"https://www.google.com/"
-	adr := *flag.String("add", "", "Addresses for request")
-	numRequest := *flag.Int("num", 20, "Number of requests")
-	//timeOut := time.Duration(*flag.Int("timeOut", 200, "Timeout of request"))
-	timeOut := time.Duration(20)
+	var addresses arrayFlags
+	flag.Var(&addresses, "address", "Addresses for request")
+	numRequest := flag.Int("num", -1, "Number of requests")
+	timeOut := time.Duration(*flag.Int("timeOut", -1, "Timeout of request"))
 	flag.Parse()
-	if adr == "" {
+	if addresses == nil {
 		panic("There are not addresses for calling")
 	}
-	adr = "https://" + adr
-	if numRequest == 5 {
-		fmt.Println("App use default value of number of requests")
+	if *numRequest < 0 {
+		*numRequest = 20
+		fmt.Println("App use default value of number of requests (20)")
 	}
-	if timeOut == 0 {
-		fmt.Println("App use default value of timeout")
+	if timeOut < 0 {
+		timeOut = time.Duration(200)
+		fmt.Println("App use default value of timeout (200)")
 	}
 	return Arguments{
-		Adr:        adr,
-		NumRequest: numRequest,
+		Addresses:  addresses,
+		NumRequest: *numRequest,
 		TimeOut:    timeOut,
 	}
 }
