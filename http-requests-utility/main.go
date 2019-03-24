cacc07cab9885170ca3d436928347ddb15fe30a4
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-03-24 11:10:40 +0300

    Refactor flag value and add additional case for failed requests. Add new structure InfoResult

--- http-requests-utility/main.go
+++ http-requests-utility/main.go
@@ -5,23 +5,35 @@ import (
 	"fmt"
 	"net/http"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 )
+
+const (
+	NegativeValue            = -1
+	DefaultValueForRequest   = 50
+	DefaultValueForTimeout   = 20
+	DefaultValueForAddresses = "https://vk.com/"
+)
+
+var initArguments Arguments
+
+type arrayFlags []string
+
+func (f *arrayFlags) String() string {
+	return fmt.Sprintf("%d", f)
+}
+
+func (f *arrayFlags) Set(value string) error {
+	*f = append(*f, value)
+	return nil
+}
 
 type Arguments struct {
 	Addresses  []string
 	NumRequest int
 	TimeOut    time.Duration
-}
-
-type Result struct {
-	Address      string
-	TotalRuntime time.Duration
-	AverageTime  time.Duration
-	MinRespTime  time.Duration
-	MaxRespTime  time.Duration
-	CountNoResp  int
 }
 
 type InfoRequests struct {
@@ -32,35 +44,20 @@ type InfoRequests struct {
 	URI          string
 }
 
-func makeRequest(client http.Client, info *InfoRequests, wg *sync.WaitGroup) {
-	defer wg.Done()
-	start := time.Now()
-	resp, err := client.Get(info.URI)
-	duration := time.Since(start)
-	info.Mutex.Lock()
-	if err != nil {
-		info.CountNoResp++
-	} else {
-		defer resp.Body.Close()
-	}
-	info.RespTime = append(info.RespTime, duration)
-	info.TotalRuntime = info.TotalRuntime + duration
-	info.Mutex.Unlock()
+type Result struct {
+	TotalRuntime time.Duration
+	AverageTime  time.Duration
+	MinRespTime  time.Duration
+	MaxRespTime  time.Duration
+	CountNoResp  int
 }
 
-type arrayFlags []string
-
-func (f *arrayFlags) String() string {
-	return fmt.Sprintf("%d", f)
+type InfoResult struct {
+	Address string
+	Result  *Result
 }
 
-func (f *arrayFlags) Set(value string) error {
-	value = "https://" + value
-	*f = append(*f, value)
-	return nil
-}
-
-func utility(args Arguments) (results []Result, arguments Arguments) {
+func utility(args Arguments) (results []InfoResult) {
 	client := &http.Client{
 		Timeout: args.TimeOut,
 	}
@@ -75,58 +72,95 @@ func utility(args Arguments) (results []Result, arguments Arguments) {
 			go makeRequest(*client, &info, &wg)
 		}
 		wg.Wait()
+		results = append(results, InfoResult{
+			Address: value,
+			Result:  getResult(info, args),
+		})
+	}
+	return
+}
+
+func makeRequest(client http.Client, info *InfoRequests, wg *sync.WaitGroup) {
+	defer wg.Done()
+	start := time.Now()
+	resp, err := client.Get(info.URI)
+	duration := time.Since(start)
+	info.Mutex.Lock()
+	if err != nil {
+		info.CountNoResp++
+	} else {
+		defer resp.Body.Close()
+		info.RespTime = append(info.RespTime, duration)
+		info.TotalRuntime = info.TotalRuntime + duration
+	}
+	info.Mutex.Unlock()
+}
+
+func getResult(info InfoRequests, arg Arguments) (res *Result) {
+	if info.CountNoResp != arg.NumRequest {
 		sort.Slice(info.RespTime, func(i, j int) bool { return info.RespTime[i] < info.RespTime[j] })
-		results = append(results, Result{
-			Address:      value,
+		res = &Result{
 			TotalRuntime: info.TotalRuntime,
 			AverageTime:  time.Duration(int(info.TotalRuntime) / len(info.RespTime)),
 			MaxRespTime:  info.RespTime[len(info.RespTime)-1],
 			MinRespTime:  info.RespTime[0],
 			CountNoResp:  info.CountNoResp,
-		})
+		}
 	}
-	arguments = args
 	return
 }
 
-func printResults(results []Result, arguments Arguments) {
-	for _, result := range results {
-		fmt.Println("Work with address ", result.Address, " is successfully done")
-		fmt.Println("Number of send requests was ", arguments.NumRequest)
-		fmt.Println("Timeout was set in ", arguments.TimeOut)
-		fmt.Println("All requests worked at ", result.TotalRuntime)
-		fmt.Println("Average time to request: ", result.AverageTime)
-		fmt.Println("Maximum return response time is ", result.MaxRespTime)
-		fmt.Println("Minimum return response time is ", result.MinRespTime)
-		fmt.Println("The number of answers were not waited: ", result.CountNoResp)
+func printResults(results []InfoResult) {
+	for _, infoResult := range results {
+		if infoResult.Result == nil {
+			fmt.Println("Work with address ", infoResult.Address, " is not done")
+			fmt.Println("All requests were failed")
+			return
+		}
+		fmt.Println("Work with address ", infoResult.Address, " is successfully done")
+		fmt.Println("Number of send requests was ", initArguments.NumRequest)
+		fmt.Println("Timeout was set in ", initArguments.TimeOut)
+		fmt.Println("All requests worked at ", infoResult.Result.TotalRuntime)
+		fmt.Println("Average time to request: ", infoResult.Result.AverageTime)
+		fmt.Println("Maximum return response time is ", infoResult.Result.MaxRespTime)
+		fmt.Println("Minimum return response time is ", infoResult.Result.MinRespTime)
+		fmt.Println("The number of answers were not waited: ", infoResult.Result.CountNoResp)
 	}
 }
 
-var initArguments Arguments
-
 func init() {
-	var addresses arrayFlags
-	flag.Var(&addresses, "address", "Addresses for request")
-	numRequest := flag.Int("num", -1, "Number of requests")
-	timeOut := flag.Int("timeout", -1, "Timeout of request")
+	//var addresses arrayFlags
+	//flag.Var(&addresses, "address", "Addresses for request")
+	var addresses []string
+	address := flag.String("addresses", "", "Addresses for request")
+	numRequest := flag.Int("num", NegativeValue, "Number of requests")
+	timeOut := flag.Int("timeout", NegativeValue, "Timeout of request")
 	flag.Parse()
-	if addresses == nil {
-		panic("There are not addresses for calling")
+	if *address == "" {
+		addresses = []string{DefaultValueForAddresses}
+		fmt.Println("App use default value of addresses: ", DefaultValueForAddresses)
+	} else {
+		addresses = strings.Split(*address, ";")
 	}
+	//if addresses == nil {
+	//	addresses = arrayFlags{DefaultValueForAddresses}
+	//	fmt.Println("App use default value of addresses: ", DefaultValueForAddresses)
+	//}
 	if *numRequest < 0 {
-		*numRequest = 20
-		fmt.Println("App use default value of number of requests (20)")
+		*numRequest = DefaultValueForRequest
+		fmt.Println("App use default value of number of requests: ", DefaultValueForRequest)
 	}
 	if *timeOut < 0 {
-		*timeOut = 200
-		fmt.Println("App use default value of timeout (200ms)")
+		*timeOut = DefaultValueForTimeout
+		fmt.Println("App use default value of timeout: ", DefaultValueForTimeout, "s")
 	}
 	initArguments = Arguments{
 		Addresses:  addresses,
 		NumRequest: *numRequest,
-		TimeOut:    time.Duration(*timeOut) * time.Millisecond,
+		TimeOut:    time.Duration(*timeOut) * time.Second,
 	}
 }
+
 func main() {
 	printResults(utility(initArguments))
 }
